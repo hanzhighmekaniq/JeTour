e99fd307731de86c4b6f5485b1758ddbcb8913e9
fadiaskeyn <fadiaskeyn@gmail.com>
2025-04-23 05:10:17 +0700

    fix: error ketika build production

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -6,9 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Jember Spot</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-CDuZlIUn.css') }}">
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BRMeKiKJ.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/pannellum.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <script src="{{ asset('assets/js/pannellum.js') }}"></script>
     <link rel="shortcut icon" href="{{ asset('assets/images/logo-footer.png') }}" type="image/x-icon">
 </head>
