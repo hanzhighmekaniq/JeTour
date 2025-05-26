e61f29d93077713eae20e6fd37e87b1bf500fea6
Dimas Dharmawan <marswan27@gmail.com>
2025-05-26 14:46:49 +0700

    menampilkan data user pada view

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -4,7 +4,7 @@
         <h1 class="text-2xl sm:text-3xl font-bold text-gray-800 mb-4 sm:mb-6">Data Pengguna</h1>
         <div class="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 pb-4">
             <div>
-               
+
             </div>
             <div class="w-full sm:w-auto">
                 <!-- Filter dan Search -->
@@ -14,55 +14,51 @@
                         placeholder="Cari nama pengguna..."
                         class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500 w-full sm:w-auto">
                     <button type="submit"
-                        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition w-full sm:w-auto"><i class="fa-solid fa-filter"></i></button>
+                        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition w-full sm:w-auto"><i
+                            class="fa-solid fa-filter"></i></button>
                 </form>
             </div>
         </div>
 
-        <!-- Tabel Wisata -->
+        <!-- Tabel Data User -->
         <div class="overflow-x-auto bg-white rounded-lg shadow w-full">
             <table class="min-w-[600px] w-full text-left text-sm">
                 <thead class="bg-blue-100 text-gray-700">
                     <tr>
-                        <th class="py-3 px-4 sm:px-6">No</th>
-                        <th class="py-3 px-4 sm:px-6">Nama Pengguna</th>
-                        <th class="py-3 px-4 sm:px-6">Email</th>
-                        <th class="py-3 px-4 sm:px-6 text-center">Aksi</th>
+                        <th class="py-3 px-4">No</th>
+                        <th class="py-3 px-4">Nama</th>
+                        <th class="py-3 px-4">Email</th>
+                        <th class="py-3 px-4 text-center">Aksi</th>
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
-                        <tr class="hover:bg-gray-100">
-                            <td class="py-3 px-4 sm:px-6"></td>
-                            <td class="py-3 px-4 sm:px-6"></td>
-                            <td class="py-3 px-4 sm:px-6"></td>
-                            <td class="py-3 px-4 sm:px-6">
-                                <span
-                                    class="inline-block px-2 py-1 text-xs text-white bg-green-500 rounded">Aktif</span>
-                            </td>
-                            <td class="py-3 px-4 sm:px-6 text-center space-x-2">
-                                <a href=""
-                                    class="inline-block mb-1 sm:mb-0">
-                                    <button
-                                        class="bg-yellow-400 hover:bg-yellow-500 text-white px-3 py-1 rounded text-xs transition">Edit</button>
+                    @forelse ($users as $index => $user)
+                        <tr>
+                            <td class="py-3 px-4">{{ $index + 1 }}</td>
+                            <td class="py-3 px-4">{{ $user->name }}</td>
+                            <td class="py-3 px-4">{{ $user->email }}</td>
+                            <td class="py-3 px-4 text-center space-x-2">
+                                <a href="{{ route('datauser.edit', $user->id) }}">
+                                    <button class="bg-yellow-400 text-white px-3 py-1 rounded text-xs">Edit</button>
                                 </a>
-                                <form action=""
-                                    method="POST" class="inline-block">
-                                    <button type="submit" onclick="return confirm('Yakin ingin menghapus?')"
-                                        class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-xs transition">Hapus</button>
+                                <form action="{{ route('datauser.destroy', $user->id) }}" method="POST"
+                                    class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button onclick="return confirm('Yakin hapus?')" type="submit"
+                                        class="bg-red-500 text-white px-3 py-1 rounded text-xs">Hapus</button>
                                 </form>
                             </td>
                         </tr>
+                    @empty
                         <tr>
-                            <td colspan="8" class="text-center py-4">Data  belum tersedia.</td>
+                            <td colspan="4" class="text-center py-4">Belum ada data.</td>
                         </tr>
+                    @endforelse
                 </tbody>
             </table>
-            <!-- Pagination -->
-            <div class="py-4 border">
-            </div>
         </div>
 
     </div>
-
 
 </x-layout>
